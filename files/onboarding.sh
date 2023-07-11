5330c0041db0a6b998dcc2a49c533d33332c3ce6
marknottage-as <mark.nottage@alaskaair.com>
2023-07-10 19:16:07 -0700

    tweak onboarding script curl options

--- files/onboarding.sh
+++ files/onboarding.sh
@@ -1,7 +1,7 @@
 rm -fr /opt/illumio_ven_data/tmp &&
 umask 026 &&
 mkdir -p /opt/illumio_ven_data/tmp &&
-curl --tlsv1 "https://us-scp7.illum.io:443/api/v22/software/ven/image?pair_script=pair.sh&profile_id=1683" -o /opt/illumio_ven_data/tmp/pair.sh &&
+curl -k --tlsv1 "https://us-scp7.illum.io:443/api/v22/software/ven/image?pair_script=pair.sh&profile_id=1683" -o /opt/illumio_ven_data/tmp/pair.sh &&
 chmod +x /opt/illumio_ven_data/tmp/pair.sh &&
 # vi /opt/illumio_ven_data/tmp/pair.sh &&
 /opt/illumio_ven_data/tmp/pair.sh --management-server us-scp7.illum.io:443 --activation-code 170083d32da56bb7b614ddac49867845eb806b58b86fa86f2629f80a69c2d6397dbf88fc26ebbe6bd
