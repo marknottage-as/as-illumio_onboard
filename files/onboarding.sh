7d4bce70aafd72db5d91df06580d8e45ba55eda9
marknottage-as <mark.nottage@alaskaair.com>
2023-07-27 11:56:44 -0700

    added --insecure to pair.sh invokation within onboarding script

--- files/onboarding.sh
+++ files/onboarding.sh
@@ -9,4 +9,4 @@ curl -k "https://us-scp7.illum.io:443/api/v22/software/ven/image?pair_script=pai
 chmod +x /opt/illumio_ven_data/tmp/pair.sh &&
 # if troubleshooting a specific endpoint, remove code that deletes pair.sh after it's run
 # vi /opt/illumio_ven_data/tmp/pair.sh &&
-/opt/illumio_ven_data/tmp/pair.sh --management-server us-scp7.illum.io:443 --activation-code 170083d32da56bb7b614ddac49867845eb806b58b86fa86f2629f80a69c2d6397dbf88fc26ebbe6bd
+/opt/illumio_ven_data/tmp/pair.sh --insecure --management-server us-scp7.illum.io:443 --activation-code 170083d32da56bb7b614ddac49867845eb806b58b86fa86f2629f80a69c2d6397dbf88fc26ebbe6bd
